8aab6cf387a4a5e4ab6852dd2b806de3ac5b1983
Giovani Del Rio <giovani@gplpm.com.br>
2019-12-11 21:46:24 -0300

    Acrescentando tracos

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -6,10 +6,12 @@ namespace projeto_teste
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("----------------------------------");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Buonanotte");
             Console.WriteLine("How r u doing?");
             Console.WriteLine("SomeTimes the deth is a kindness");
+            Console.WriteLine("-----------------------------------");
         }
     }
 }
